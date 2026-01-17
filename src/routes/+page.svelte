c5c9749dc74a2fc3b351564023fae67cb7cd27b7
Goatmanwaffles <70929003+Goatmanwaffles@users.noreply.github.com>
2026-01-17 01:27:32 -0500

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Orbit from '$lib/orbit.svelte';
+</script>
+
+<div class="bg-gray-800 h-screen w-full relative overflow-hidden">
+  <Orbit />
+  <div class="relative z-20 flex flex-col items-center justify-center gap-6 h-screen w-full">
+    <h1 class="text-5xl font-bold text-pink-500 col-span-2">Logan Senol</h1>
+    <p class="text-gray-400 text-xl font-bold">A Full Stack Developer</p>
+    <div class="flex flex-row gap-32">
+      <a href="/about" class="rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-500 transition-colors duration-200">&lt;About me&gt;</a>
+      <a href="/projects" class="rounded-full bg-pink-400 px-14 py-2 hover:bg-pink-500 transition-colors duration-200">&lt;My Projects&gt;</a>
+    </div>
+  </div>
+</div>
